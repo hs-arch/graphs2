7ace21160a73effa09f2023ac8a9ad5a2ceba2b0
HamidSarfaraz <hamidsarfraz8@gmail.com>
2023-01-07 18:12:18 +0530

    DFS traversal on graph

--- DFS_traversal.java
+++ DFS_traversal.java
@@ -1,59 +1,57 @@
-import java.util.ArrayList;
-import java.util.Map;
+// DFS algorithm in Java
 
-public class adjacencyList {
-	
-	public static void addEdge(ArrayList<ArrayList<Integer>> graph, int node1, int node2, boolean isDirected){
-		
-		if(isDirected == false){
-			graph.get(node1).add(node2);
-			graph.get(node2).add(node1);
-		}
-		else {
-			graph.get(node1).add(node2);
-		}
-	}
-	
+import java.util.*;
 
-	//Lets print the graph to see the adjacencyList of the Edges for each nodes.
-	public static void printEdges(ArrayList<ArrayList<Integer>> graph) {
-		for(int i=0;i<graph.size();i++) {
-			System.out.print("Vertex : " + i + " -> ");
-			//System.out.println();
-			for(int j=0;j<graph.get(i).size();j++)
-			{
-				System.out.print(graph.get(i).get(j)+" - ");
-			}
-			System.out.println();
-		}
-	}
-	
-	
-	public static void main(String[] args) {
-	
-		//Creating the graph
-		int N = 5;
-		ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
-		
-		for(int i=0;i<N;i++) {
-			graph.add(new ArrayList<Integer>());
-		}
-		//Now from the above code we have a graph with N=5 nodes.
-		
-		
-		//Now we add edges to nodes. For that we create an addEdge function
-		
-		//adding edge between 0 and 1 in list(list is our graph actually)
-		// false -> undirected graph
-		// true -> directed graph
-		addEdge(graph,0,1,false);
-		addEdge(graph,0,2,false);
-		addEdge(graph,0,3,false);
-		addEdge(graph,1,2,false);
-		addEdge(graph,3,1,false);
-		
-		printEdges(graph);
-		
-	}
-	
+class DFS_traversal {
+  private LinkedList<Integer> adjLists[];
+  private boolean visited[];
+
+  // Graph creation
+  DFS_traversal(int vertices) {
+    adjLists = new LinkedList[vertices];
+    visited = new boolean[vertices];
+
+    for (int i = 0; i < vertices; i++)
+      adjLists[i] = new LinkedList<Integer>();
+  }
+
+  // Add edges
+  void addEdge(int src, int dest) {
+    adjLists[src].add(dest);
+  }
+
+  // DFS algorithm
+  void DFS(int vertex) {
+    visited[vertex] = true;
+    System.out.print(vertex + " ");
+
+    Iterator<Integer> ite = adjLists[vertex].listIterator();
+    while (ite.hasNext()) {
+//    	System.out.println("ite = " + ite.next());
+      int adj = ite.next();
+      System.out.println("ite value" + adj);
+      if (!visited[adj])
+        DFS(adj);
+    }
+  }
+
+  public static void main(String args[]) {
+    DFS_traversal g = new DFS_traversal(4);
+
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 2);
+    g.addEdge(2, 3);
+
+    System.out.println("Following is Depth First Traversal");
+    
+//    try {
+//		System.out.println("ADJ List" + adjLists[0]);
+//	} catch (Exception e) {
+//		// TODO Auto-generated catch block
+//		e.printStackTrace();
+//	}
+
+    g.DFS(0);
+  }
 }
