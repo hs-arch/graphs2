de1c212f55da5cdbd2253fe3d72691dbc7365e51
HamidSarfaraz <hamidsarfraz8@gmail.com>
2023-01-07 18:29:06 +0530

    first commit of this problem

---
+++ cycle_detection_DFS_traversal.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+class cycle_detetion_DFS_traversal{
+
+    public void addEdge(int src, int dest){
+        
+    }
+
+    public static void main(String args[]){
+        System.out.println("Hello");
+
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
+
+        for(int i=0;i<10;i++){
+            graph.add(new ArrayList<Integer>());
+        }
+
+        graph.addEdge(0,1);
+    }
+}
